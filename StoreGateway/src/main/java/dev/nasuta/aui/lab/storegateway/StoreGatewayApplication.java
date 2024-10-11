3ee7564885c3ad1410594e50d6b7a2e31a918231
Krzysztof Nasuta <krzysztof@nasuta.dev>
2024-10-11 20:24:44 +0200

    feat: add api gateway

---
+++ StoreGateway/src/main/java/dev/nasuta/aui/lab/storegateway/StoreGatewayApplication.java
@@ -0,0 +1,54 @@
+package dev.nasuta.aui.lab.storegateway;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class StoreGatewayApplication {
+    private final String storeGatewayHost;
+    private final String categoryServiceUrl;
+    private final String productServiceUrl;
+
+    public StoreGatewayApplication(
+            @Value("${store.gateway.host}") String storeGatewayHost,
+            @Value("${store.category-service.url}") String categoryServiceUrl,
+            @Value("${store.product-service.url}") String productServiceUrl
+    ) {
+        this.storeGatewayHost = storeGatewayHost;
+        this.categoryServiceUrl = categoryServiceUrl;
+        this.productServiceUrl = productServiceUrl;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator storeRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route("categories", route -> route
+                        .host(storeGatewayHost)
+                        .and()
+                        .path(
+                                "/api/categories",
+                                "/api/categories/{uuid}"
+                        )
+                        .uri(categoryServiceUrl)
+                )
+                .route("products", route -> route
+                        .host(storeGatewayHost)
+                        .and()
+                        .path(
+                                "/api/products",
+                                "/api/products/{uuid}",
+                                "/api/categories/{uuid}/products"
+                        )
+                        .uri(productServiceUrl)
+                )
+                .build();
+    }
+}
---
+++ StoreGateway/src/test/java/dev/nasuta/aui/lab/storegateway/StoreGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nasuta.aui.lab.storegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
